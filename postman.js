8eb65813591c5754c211cbebdc408c684ad56a2e
SHRUTI VINODKUMAR PARMAR <2403031590269@paruluniversity.ac.in>
2026-07-07 23:07:11 +0530

    Update postman.js

--- postman.js
+++ postman.js
@@ -1,4 +1,4 @@
-//get
+//put
 
 fetch("https://jsonplaceholder.typicode.com/posts/1",{  
     method:"PUT",
@@ -14,4 +14,23 @@ fetch("https://jsonplaceholder.typicode.com/posts/1",{
 .then((res)=>res.json())
 .then((val)=>{
     console.log(val);
+})
+
+
+//POST
+fetch("https://jsonplaceholder.typicode.com/posts",{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json",
+  },
+  body:JSON.stringify({
+    "name":"Mern batch",
+    "batch-no":1,
+    "id":1
+  })
+
+})
+.then((res)=>res.json())
+.then((val)=>{
+  console.log(val)
 })
